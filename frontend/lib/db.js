362a1db139450143b8d11209b166440c0ba47807
Tejas Mhaske <tejas.ycmku22@sinhgad.edu>
2026-07-09 22:47:53 +0530

    Mysql hosting

--- frontend/lib/db.js
+++ frontend/lib/db.js
@@ -1,20 +1,14 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-
-    host: "localhost",
-
-    user: "root",
-
-    password: "root123",
-
-    database: "investor_portal",
-
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-
     connectionLimit: 10,
-
-    queueLimit: 0
+    queueLimit: 0,
 });
 
 export default pool;
